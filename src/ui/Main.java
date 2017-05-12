afca1d41a30729bb2f00011018177405e17efcae
billy0920 <billy.wuweibin@gmail.com>
2017-05-12 22:43:35 +0800

    change show() to setVisible(true).

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new MainWin("NetSimu").show();
+		new MainWin("NetSimu").setVisible(true);
 	}
 
 }
